e70d8d4a598a02a2b18a3167d5e9a72efef2a877
Lesego0773 <kabelomakokga@gmail.com>
2024-04-17 11:49:23 -0700

    tt

--- models/state.py
+++ models/state.py
@@ -8,4 +8,4 @@ from sqlalchemy.orm import relationship
 class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")i
+    cities = relationship("City", backref="state")
