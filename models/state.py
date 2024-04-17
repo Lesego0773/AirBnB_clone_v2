0e0c01513785257ef72e44773c50a3c7ec5ca629
Lesego0773 <kabelomakokga@gmail.com>
2024-04-17 11:37:31 -0700

    kk

--- models/state.py
+++ models/state.py
@@ -1,11 +1,36 @@
 #!/usr/bin/python3
 
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+# models/base_model.py
 
-class State(BaseModel, Base):
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String
+from sqlalchemy import MetaData
+import uuid
+
+Base = declarative_base()
+
+class BaseModel:
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    updated_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+
+    def __init__(self, *args, **kwargs):
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+
+    def save(self):
+        self.updated_at = datetime.utcnow()
+        storage.save()
+
+    def delete(self):
+        storage.delete(self)
+
+    def to_dict(self):
+        dictionary = self.__dict__.copy()
+        dictionary.pop('_sa_instance_state', None)
+        return dictionary
 
